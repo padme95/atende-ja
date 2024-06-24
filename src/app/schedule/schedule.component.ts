4e1e3befbc7880d06995c7720e63734751c3538b
Manuel Bravo <manuelbravoc@gmail.com>
2024-06-23 23:22:42 -0300

    Normalizacao

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
-import { environment } from '../../environments/environment';
+import { faHome, faUser, faCalendar, faUsersLine, faLaptopMedical} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-schedule',
-  standalone: true,
-  imports: [GoogleMapsModule],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css'
 })
 export class ScheduleComponent {
   center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
   zoom = 4;
+
+  faHome = faHome;
+  faCalendar = faCalendar
+  faUser = faUser;
+  faLaptopMedical = faLaptopMedical
+  faUsersLine = faUsersLine;
 }
